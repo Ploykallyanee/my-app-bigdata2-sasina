06c3691afe4d5e2694dd1ef5ebad15f277fba512
sasinaaell <sasinaaell@gmail.com>
2024-07-19 14:43:39 +0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import LandingPage from "@/components/LandingPage";
+import React from "react";
+
+export default function home() {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  );
+}
